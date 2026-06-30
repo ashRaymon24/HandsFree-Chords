3dfcb5787fb77c648fb4f6cb7b0f0a6c64620539
Asher Raymon <ashraymon24@gmail.com>
2026-06-30 18:42:21 +0100

    Implemented basic webcam program

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HandsFree-Chords
 Navigate browser chords using head gestures
+
+Milestones:
+    Webcam detection
+    Face tracking
+    Head pose estimation
+    Nod detection
+    AI parser
+    Browser extension
+    Settings page
---
+++ webcam.py
@@ -0,0 +1,19 @@
+import cv2
+
+stream = cv2.VideoCapture(0)
+
+if not stream.isOpened():
+    print("No Stream")
+    exit()
+
+while True:
+    ret, frame = stream.read()
+    if not ret:
+        print("No more stream")
+        break
+    cv2.imshow("Webcam", frame)
+    if cv2.waitKey(1) == ord("q"): #stops webcam video when q key is pressed
+        break
+
+stream.release()
+cv2.destroyAllWindows()
